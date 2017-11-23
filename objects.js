46f400b6a5b774e7b81d7ef71e8c45f4b1d9e86f
jpkim921 <jpkim921@gmail.com>
2017-11-23 16:29:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -15,11 +15,11 @@ function removeFromPlaylist(playlist, artistName){
 }
 
 // var playlist = {'Jay Z': 'Monster'};
-// 
+//
 // function updatePlaylist(object, key, value){
 // return Object.assign({}, object, { [key]: value})
 // }
-// 
+//
 // function removeFromPlaylist(playlist, artistName){
 //   delete playlist[artistName]
 //   return playlist
